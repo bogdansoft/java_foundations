09702cf0cd6ccd928f0cad2fd3df56e3d08ff3b0
bsyso <bsyso@softserveinc.com>
2022-08-23 13:07:44 +0200

    added regex comments

--- src/main/java/regex/NamedCaptureGroupsRegExClass.java
+++ src/main/java/regex/NamedCaptureGroupsRegExClass.java
@@ -5,19 +5,25 @@ import java.util.regex.Pattern;
 
 public class NamedCaptureGroupsRegExClass {
     public static void main(String[] args) {
-        String regex = "(?:(?<countryCode>\\d{1,2})[-.,\\s]?)?(?:(?<areaCode>\\d{3})[-.,\\s]?)(?:(?<exchange>\\d{3})[-.,\\s]?)(?<line>\\d{4})";
+        String regex = """
+                # This is my regex expression
+                (?:(?<countryCode>\\d{1,2})[-.,\\s]?)? # Gets country code
+                (?:(?<areaCode>\\d{3})[-.,\\s]?) # Gets area code
+                (?:(?<exchange>\\d{3})[-.,\\s]?) # Gets exchange
+                (?<lineNumber>\\d{4}) # Gets line number
+                """;
         String phoneNumber = "24.523.343.3432";
 
         System.out.println(phoneNumber.matches(regex));//true
 
-        Pattern pattern = Pattern.compile(regex);
+        Pattern pattern = Pattern.compile(regex, Pattern.COMMENTS);
         Matcher matcher = pattern.matcher(phoneNumber);
 
         if (matcher.matches()) {
             System.out.format("Country code: %s\n", matcher.group("countryCode"));
             System.out.format("Area code: %s\n", matcher.group("areaCode"));
             System.out.format("Exchange: %s\n", matcher.group("exchange"));
-            System.out.format("Line number: %s\n", matcher.group("line"));
+            System.out.format("Line number: %s\n", matcher.group("lineNumber"));
         }
     }
 }
