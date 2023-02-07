25232c0ebd1d5e2672541ff960c3cce1ec014c16
bsyso <bsyso@softserveinc.com>
2023-02-07 12:41:17 +0100

    added modularity

---
+++ my-another-module/src/main/java/org/another/Bye.java
@@ -0,0 +1,7 @@
+package org.another;
+
+public class Bye {
+    public static void main(String[] args) {
+        System.out.println("Bye world! from " + Bye.class);
+    }
+}
---
+++ my-module/src/main/java/org/modularity/Hello.java
@@ -0,0 +1,10 @@
+package org.modularity;
+
+import org.another.Bye;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world! from " + Hello.class);
+        System.out.println("Bye from another module " + Bye.class);
+    }
+}
